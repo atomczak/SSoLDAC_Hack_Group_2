b0a97676d8aa5224438922a3bad02385bb135a7e
Ruben Kruiper <rubenkruiper@gmail.com>
2023-07-03 10:59:38 +0100

    updating calls to SPaR.txt after changing the docker container there

--- utilities/spar_utils.py
+++ utilities/spar_utils.py
@@ -19,21 +19,9 @@ class NER:
         self.api = api
         self.split_length = split_length   # in number of tokens
     
-    def process_sentence(self, sentence: str = ''):
-        """
-        """
-        # SPaR doesn't handle ALL uppercase sentences well, which you might get as output from OCR    
-        sentence = sentence.lower() if sentence.isupper() else sentence
-        prediction_dict =  requests.post(self.api,  json={"sentence": sentence}).json()
-        if not prediction_dict:
-            return []
-
-        pred_labels = prediction_dict["prediction"]
-        # only returing the objects (obj) for now
-        return pred_labels['obj']
-    
     def split_into_sentences(self, to_be_split: Union[str, List[str]]) -> List[str]:
         """
+        Split a larger text into sentences using TextBlob (NLTK's PunktSentTokenizer).
         """
         if type(to_be_split) == str:
             if ';' in to_be_split:
@@ -47,21 +35,18 @@ class NER:
             for part in text.split('\n'):
                 # split into sentences using PunktSentTokenizer (TextBlob implements NLTK's version under the hood) 
                 sentences += [str(s) for s in TextBlob(part.strip()).sentences if len(str(s)) > 10]
+                
         return sentences
-    
-    def process_sentences(self, sentences: List[str]):
-        """
-        """
-        spar_objects = []
-        for sentence in sentences:
-            # Note; we are expecting a single list of objects here!
-            spar_objects += self.process_sentence(sentence)
-        return spar_objects
     
     def process_text(self, text: Union[str, List[str]]):
         """
+        Call the SPaR.txt API, expecting {"texts": original_input, "sentences": List[str], "predictions": List[Dict[str, str]]}
         """
-        sentences = self.split_into_sentences(text)
-        return self.process_sentences(sentences)
+        response = requests.post(self.api,  json={"texts": text}).json()
+        # texts = response['texts']
+        sentences = response['sentences']
+        predictions = response['predictions']
+        
+        return sentences, predictions
         
         
